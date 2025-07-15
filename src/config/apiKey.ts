5d995f17de18f7d61951586148338126c12f1da2
Keran Sanjay <keransanjay88ifs@gmail.com>
2025-07-15 20:03:17 +0530

    1

--- src/config/apiKey.ts
+++ src/config/apiKey.ts
@@ -1,2 +1,2 @@
 // Get your API key from https://makersuite.google.com/app/apikey
-export const GEMINI_API_KEY = 'AIzaSyCGBYvpTKirNypNnh3fDEE5QFk1dgEOiAM'; // Your API key should be inside single or double quotes
+export const GEMINI_API_KEY = 'YOUR-API-KEY'; // Your API key should be inside single or double quotes
